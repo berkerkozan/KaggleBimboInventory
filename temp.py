8d7b0a7773f1b518986656f3e4a0c8a477fc8186
berkerkozan <berkerkozan@gmail.com>
2016-06-17 19:14:52 +0300

    Update temp.py

--- temp.py
+++ temp.py
@@ -50,5 +50,5 @@ df.to_csv("train_sample.csv")
 #qweqweqweqweqweqweqwe
 #qqqqqqq
 #aaaa
-#f.close()qweqweqweqwe22233444666677799911113333
+#f.close()qweqweqweqwe222334446666777999111133334444
 #o.close()
