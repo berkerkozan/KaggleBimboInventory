969e7f4469970b340be9942913dec8f24cd641f4
cankoklu <cankoklu@gmail.com>
2016-07-18 18:45:55 +0300

    xgboost works. .77 score need to add client id in columns (and/or play with xgboost parameters)

--- 15_xgboost_simple.py
+++ 15_xgboost_simple.py
@@ -1,17 +1,20 @@
-import numpy as np
-from numpy import *
-import xgboost as xgb
-import pandas as pd
-import math
-
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import nltk.corpus
 from nltk.stem.snowball import SnowballStemmer
-
 from sklearn.cross_validation import train_test_split
-
 from ml_metrics import rmsle
-
-import scipy.sparse as sp
+import xgboost as xgb
+import math
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import datasets, linear_model
+import scipy.sparse as sps
+from scipy.sparse import coo_matrix, hstack, vstack, csr_matrix
+from scipy import io
+from datetime import datetime
+import gc
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 
@@ -27,10 +30,27 @@ def evalerror(preds, dtrain):
     terms_to_sum = [(math.log(labels[i] + 1) - math.log(max(0,preds[i]) + 1)) ** 2.0 for i,pred in enumerate(labels)]
     return 'error', (sum(terms_to_sum) * (1.0/len(preds))) ** 0.5
 
-train = pd.read_csv('../input/train_sample_100.csv')
-small_temp_test = pd.read_csv('../input/test_sample_100.csv')
-temp_test = pd.read_csv('../input/test.csv', chunksize=100000)
+# train = pd.read_csv('../input/train_sample_10000.csv')
+# test = pd.read_csv('../input/test_sample_10000.csv')
+#temp_test = pd.read_csv('../input/test.csv', chunksize=100000)
 
+
+# train = pd.read_csv('../input/train_1000000.csv', usecols=['Semana', 'Agencia_ID',
+#                                                       'Canal_ID', 'Cliente_ID', 'Producto_ID', 'Demanda_uni_equil'])
+#
+# test = pd.read_csv('../input/test_1000000.csv',
+#                    usecols=['id', 'Semana', 'Agencia_ID', 'Canal_ID', 'Cliente_ID', 'Producto_ID']
+#                    )
+
+
+train = pd.read_csv('../input/train.csv', usecols=['Cliente_ID', 'Producto_ID', 'Demanda_uni_equil'])
+test = pd.read_csv('../input/test.csv', usecols=['id', 'Cliente_ID', 'Producto_ID'])
+
+# train = pd.read_csv('../input/train.csv', usecols=['Cliente_ID', 'Producto_ID', 'Demanda_uni_equil'])
+# test = pd.read_csv('../input/test.csv', usecols=['id', 'Cliente_ID', 'Producto_ID'])
+
+# train = pd.read_csv('../input/train.csv')
+# test = pd.read_csv('../input/test.csv')
 
 # Process product data.
 
@@ -42,7 +62,7 @@ products['short_name'] = products.NombreProducto.str.extract('^(\D*)', expand=Fa
 # products['brand'] = products.NombreProducto.str.extract('^.+\s(\D+) \d+$', expand=False)
 # w = products.NombreProducto.str.extract('(\d+)(Kg|g)', expand=True)
 # products['weight'] = w[0].astype('float') * w[1].map({'Kg': 1000, 'g': 1})
-products['pieces'] = products.NombreProducto.str.extract('(\d+)p ', expand=False).astype('float')
+# products['pieces'] = products.NombreProducto.str.extract('(\d+)p ', expand=False).astype('float')
 
 products['short_name_processed'] = (products['short_name'].map(
     lambda x: " ".join([i for i in x.lower().split() if i not in nltk.corpus.stopwords.words("spanish")])))
@@ -52,9 +72,23 @@ products['short_name_processed'].map(lambda x: " ".join([stemmer.stem(i) for i i
 
 short_name_processed_list = products['short_name_processed'].unique()
 
+# products = products.drop(['short_name', 'short_name_processed', 'NombreProducto'], axis=1)
+# product_ids = products.astype(np.uint16)
+#
+# product_short_names =
 
 products = pd.concat([products.drop(['short_name', 'short_name_processed', 'NombreProducto'], axis=1), pd.get_dummies(short_name_processed_list)], axis=1)
+products = products.drop([''], axis=1)
 
+bool_cols = products.columns[1:]
+products['Producto_ID'] = products['Producto_ID'].astype(np.uint16, errors='coerce')
+products[bool_cols] = products[bool_cols].fillna(0.0).astype(np.int8, errors='coerce')
+
+
+# products = products.astype(np.int8, )
+# products.fillna(value=0, inplace=True)
+
+print('products shape:', products.shape)
 print('product features generated')
 
 # Join data and products
@@ -63,45 +97,13 @@ train.fillna(value=0, inplace=True)
 
 print('train data joined')
 
-
-
-# test = pd.DataFrame()
-
-
-
-print('real test created')
-
-count = 0
-for chunk in temp_test:
-
-
-    df_chunk = pd.DataFrame(chunk)
-    df_chunk = df_chunk.join(products, on='Producto_ID', lsuffix='_t')
-    # array_chunk = array(df_chunk)
-    csr_chunk = sp.csr_matrix(df_chunk)
-    # csr_chunk = sps.csr_matrix(array_chunk)
-    # csr_chunk = sps.csr_matrix((df_chunk, indices, indptr), [shape = (M, N)])
-    if count == 0:
-        test = csr_chunk
-    else:
-        test = sp.vstack((test, csr_chunk), format='csr')
-
-    # test = pd.concat([test, df_chunk], axis=1)
-    #test = test.append(df_chunk, ignore_index=True)
-    count += 1
-    print('test shape', test.shape)
-    print('chunk shape', df_chunk.shape)
+test = test.join(products, on='Producto_ID', lsuffix='_t')
+test.fillna(value=0, inplace=True)
 
 print('test data joined')
+print('train shape', train.shape)
 print('test shape', test.shape)
-print('chunk shape', df_chunk.shape)
 
-
-print('product features added to table')
-
-
-print ('')
-print ('Training_Shape:', train.shape)
 
 ids = test['id']
 test = test.drop(['id'],axis = 1)
@@ -141,6 +143,8 @@ fold_preds = np.around(xgclassifier.predict(fxg_test, ntree_limit=xgclassifier.b
 test_preds += fold_preds
 
 submission = pd.DataFrame({'id':ids, 'Demanda_uni_equil': test_preds})
-submission.to_csv('submission.csv', index=False)
+
+submission[["id","Demanda_uni_equil"]].to_csv('../submissions/' +
+                                              datetime.now().strftime('%Y-%m-%d-%H-%M-%S') +'.csv', index=False)
 
 print ('done')
